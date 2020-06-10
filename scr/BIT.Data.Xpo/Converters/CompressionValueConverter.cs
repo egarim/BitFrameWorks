2784d52560d04d6db8e42e6a40258f9e3896ce94
Jose Ojeda <joche.ojeda@bitframeworks.com>
2020-06-09 19:50:21 -0700

    netcoreapp3.1

--- scr/BIT.Data.Xpo/Converters/CompressionValueConverter.cs
+++ scr/BIT.Data.Xpo/Converters/CompressionValueConverter.cs
@@ -1,38 +1,43 @@
 ﻿using DevExpress.Xpo.Metadata;
 using System;
 using System.IO;
-
+using BIT.Data.Helpers;
 namespace BIT.Data.Helpers
 {
-    public class CompressionValueConverter : ValueConverter
-    {
-        public override object ConvertToStorageType(object value)
-        {
-            if (value != null && !(value is byte[]))
-            {
-                throw new ArgumentException();
-            }
-            if (value == null || ((byte[])value).Length == 0)
-            {
-                return value;
-            }
-            return CompressionHelper.Compress(new MemoryStream((byte[])value)).ToArray();
-        }
-        public override object ConvertFromStorageType(object value)
-        {
-            if (value != null && !(value is byte[]))
-            {
-                throw new ArgumentException();
-            }
-            if (value == null || ((byte[])value).Length == 0)
-            {
-                return value;
-            }
-            return CompressionHelper.Decompress(new MemoryStream((byte[])value)).ToArray();
-        }
-        public override Type StorageType
-        {
-            get { return typeof(byte[]); }
-        }
-    }
+    //HACK todo fix
+    //public class CompressionValueConverter : ValueConverter
+    //{
+    //    CompressionHelper
+    //    public CompressionValueConverter()
+    //    {
+    //    }
+    //    public override object ConvertToStorageType(object value)
+    //    {
+    //        if (value != null && !(value is byte[]))
+    //        {
+    //            throw new ArgumentException();
+    //        }
+    //        if (value == null || ((byte[])value).Length == 0)
+    //        {
+    //            return value;
+    //        }
+    //        return CompressionHelper.Compress(new MemoryStream((byte[])value)).ToArray();
+    //    }
+    //    public override object ConvertFromStorageType(object value)
+    //    {
+    //        if (value != null && !(value is byte[]))
+    //        {
+    //            throw new ArgumentException();
+    //        }
+    //        if (value == null || ((byte[])value).Length == 0)
+    //        {
+    //            return value;
+    //        }
+    //        return CompressionHelper.Decompress(new MemoryStream((byte[])value)).ToArray();
+    //    }
+    //    public override Type StorageType
+    //    {
+    //        get { return typeof(byte[]); }
+    //    }
+    //}
 }
--- scr/BIT.Data/Helpers/ConfigurationResolverBase.cs
+++ scr/BIT.Data/Helpers/ConfigurationResolverBase.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿
 
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.IO;
